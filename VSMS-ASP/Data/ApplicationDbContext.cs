0a6d0b77b0fe295829860f9f8dc34eed88d0a64c
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-02-28 17:44:36 +0200

    Add project files.

---
+++ VSMS-ASP/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VSMS_ASP.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
