d115f7fe4282f58729bffca4abf333aaf074b804
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-03-04 10:36:08 +0200

    Laying down the arch foundation

---
+++ VSMS.Core/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSMS.Core
+{
+    public static class MessageConstants
+    {
+        public static string ErrorMessage = "Името или паролата е невалидна!";
+        public static string SuccessMessage = "Операцията е изпълнена!"; 
+        public static string WarningMessage = "Неуспешно изпълнена заявка!";
+    }
+}
---
+++ VSMS.Core/Contracts/IUserService.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace VSMS.Core.Contracts
+{
+    public interface IUserService
+    {
+        //bool Register(RegisterViewModel model);
+        //string Login(LoginViewModel model);
+        string GetUsername(string userId);
+        //(string userId, bool isCorrect) IsLoginCorrect(LoginViewModel model);
+    }
+}
