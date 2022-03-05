cc41c3d7dfcb4326d01d231fa34a4ccc871e13dd
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-03-05 18:00:57 +0200

    Created a method for extracting the Role Power of users.

--- VSMS.Core/Contracts/IUsersService.cs
+++ VSMS.Core/Contracts/IUsersService.cs
@@ -5,8 +5,9 @@ namespace VSMS.Core.Contracts
     public interface IUsersService
     {
         //bool CreateUser(RegisterViewModel model);
-        (string error, bool isLogged) LogIn(LoginViewModel model);
+        string LogIn(LoginViewModel model);
         string GetUsername(string userId);
         (string tempData, bool isCorrect) IsLoginCorrect(LoginViewModel model);
+        int getRolePower(string Username);
     }
 }
--- VSMS.Core/Services/UsersService.cs
+++ VSMS.Core/Services/UsersService.cs
@@ -1,18 +1,38 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using VSMS.Core.Contracts;
 using VSMS.Core.ViewModels;
+using VSMS.Infrastructure.Data.Common;
+using VSMS.Infrastructure.Data.Models;
 
 namespace VSMS.Core.Services
 {
     public class UsersService : IUsersService
     {
+
+        private readonly Repository repo;
+
+        public UsersService(Repository _repo)
+        { repo = _repo; }
+
         public string GetUsername(string userId)
         {
             throw new NotImplementedException();
+        }
+
+        public int getRolePower(string Username)
+        {
+            var power = repo.All<Users>()
+                .Where(x => x.Username == Username)
+                .Include(r => r.Role).Where(r=>r.RoleId==r.Role.Id)
+                .Select(r=>r.Role.Power)
+                .FirstOrDefault();
+           return power;
         }
 
         public (string tempData, bool isCorrect) IsLoginCorrect(LoginViewModel model)
@@ -22,9 +42,27 @@ namespace VSMS.Core.Services
             else { return (null, true); }
         }
 
-        public (string error, bool isLogged) LogIn(LoginViewModel model)
+        public string LogIn(LoginViewModel model)
         {
-            throw new NotImplementedException();
+            string passHash = CalculateHash(model.Password);
+
+            var user = repo.All<Users>()
+                 .Where(u => u.Username == model.UserName)
+                 .Where(u => u.Password == CalculateHash(model.Password))
+                 .SingleOrDefault();
+            Console.WriteLine(user);
+
+            return user?.Id;
+        }
+
+        private string CalculateHash(string password)
+        {
+            byte[] passworArray = Encoding.UTF8.GetBytes(password);
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                return Convert.ToBase64String(sha256.ComputeHash(passworArray));
+            }
         }
     }
 }
