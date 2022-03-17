c9368f41973bdfcf20d7a7586effd5b2ff3ec52f
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-03-17 20:21:34 +0200

    Added  a password reset page

---
+++ VSMS-ASP/Controllers/HelpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VSMS_ASP.Controllers
+{
+    public class HelpController : Controller
+    {
+        public IActionResult ForgottenLogin()
+        {
+            return View();
+        }
+    }
+}
