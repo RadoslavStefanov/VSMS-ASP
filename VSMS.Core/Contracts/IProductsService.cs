f6e150a24b7e28a00f6273054c5c838bbd6232f7
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-03-23 12:47:47 +0200

    Started developing the Products handling

---
+++ VSMS.Core/Contracts/IProductsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSMS.Core.Contracts
+{
+    public interface IProductsService
+    {
+        public void Delete(string arg);
+        public void Create()
+    }
+}
