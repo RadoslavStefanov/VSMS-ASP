37f536254b7b54788c02add2c3e3d8db0235bdb7
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-03-24 00:23:59 +0200

    Removed the old Seeding

--- VSMS-ASP/Areas/Identity/Data/VSMS_ASPContext.cs
+++ VSMS-ASP/Areas/Identity/Data/VSMS_ASPContext.cs
@@ -18,20 +18,6 @@ public class VSMS_ASPContext : IdentityDbContext<IdentityUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
-        Seed();
     }
 
-    protected void Seed()
-    {
-        var passwordHash = new PasswordHasher();
-        string password = passwordHash.HashPassword("123123");
-        this.Users.Add
-            (new IdentityUser
-            {
-                UserName = "admin@virtus.bg",
-                PasswordHash = password,
-                Email = "admin@virtus.bg"
-            });
-        this.Roles.Add(new IdentityRole { Id = "1", Name = "Admin" });
-    }
 }
