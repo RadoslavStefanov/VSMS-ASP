065a1a9fb801b69c1082fd293d1347978f27ec88
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-03-29 16:46:02 +0300

    Added a contacts page

---
+++ VSMS-ASP/Controllers/ContactsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VSMS_ASP.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Show()
+        {return View();}
+    }
+}
