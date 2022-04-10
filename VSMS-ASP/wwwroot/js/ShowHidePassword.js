4617dfdbf7deffcdeb01292d0e8054e0857f3b54
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-04-10 14:00:10 +0300

    Added show/hide password

---
+++ VSMS-ASP/wwwroot/js/ShowHidePassword.js
@@ -0,0 +1,10 @@
+﻿let passwordControl = document.getElementById("passwordControl");
+passwordControl.addEventListener('click', showHidePassword)
+
+let passwordField = document.getElementById("password");
+
+function showHidePassword()
+{
+    if (passwordField.type == "password") { passwordField.type = "text"; passwordControl.innerText="Скрии паролата"}
+    else { passwordField.type = "password"; passwordControl.innerText = "Покажи паролата" }
+}
