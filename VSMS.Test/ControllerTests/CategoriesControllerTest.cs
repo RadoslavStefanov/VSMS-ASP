8d359b3c3ff229cfb7f6c04a8c7a7311d6e0a870
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-04-15 16:58:37 +0300

    Continuing with testing

---
+++ VSMS.Test/ControllerTests/CategoriesControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using VSMS_ASP.Controllers;
+
+namespace VSMS.Test.ControllerTests
+{
+    public class CategoriesControllerTest
+    {
+        [Test]
+        public void ShouldNotThrowWhenCalled()
+        {
+            var controller = new ContactsController();
+            var result = controller.Show() as ViewResult;
+            Assert.AreEqual("~/Views/Contacts/Show.cshtml", result.ViewName);
+        }
+    }
+}
